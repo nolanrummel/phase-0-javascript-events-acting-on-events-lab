721b132091d6781f52600d2b161132256403e7c6
Nolan Rummel <nolan.rummel22@gmail.com>
2023-05-15 20:47:06 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Your code here
+let dodger = document.getElementById("dodger");
+
+document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+        moveDodgerLeft();
+    }
+    if (e.key === "ArrowRight") {
+        moveDodgerRight();
+    }
+});
+
+function moveDodgerLeft() {
+    let left = parseInt(dodger.style.left.replace("px", ""));
+
+    if (left > 0) {
+        dodger.style.left = `${left - 1}px`;
+    }
+};
+
+function moveDodgerRight() {
+    let left = parseInt(dodger.style.left.replace("px", ""));
+
+    if (left < 360) {
+        dodger.style.left = `${left + 1}px`;
+    }
+};
